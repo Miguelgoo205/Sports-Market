7fd34d27c1d64bdf43ca6c11df19075d478aba06
Miguel Ibarra <miguelgoo205@gmail.com>
2022-05-03 23:10:06 -0500

    feat: Se realizaron cambios en los estilos y se agregaron botones de eliminar y comprar. M.A

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
-const main = document.getElementById("main-market");
+const main = document.getElementById('main-market');
 const selectProducts = document.getElementById("select-products");
-// products.map(elemento=>{console.log(element.name)});
-window.addEventListener('load',loadSelect);
+
+
+window.addEventListener('load', loadSelect);
+selectProducts.addEventListener('change', elementSelected);
+
+function elementSelected(event) {
+  // 1. Recorrer el array
+products.map(element => {
+    if (event.target.value === element.name) {
+    nameProduct = element.name;
+    imgProduct = element.img;
+    priceProduct = element.price;
+    }
+})
+createCard(nameProduct, imgProduct, priceProduct)
+
+}
+
 function loadSelect() {
     products.map(element=>{
     const opt = document.createElement('option');
     opt.textContent = element.name;
     opt.value = element.name;
     selectProducts.appendChild(opt);
-    });
-} 
+    });    
+}
+
+function createCard(nameProduct, imgProduct, priceProduct) {
+    const card = document.createElement('div');
+    const imgCard = document.createElement('img');
+    const title = document.createElement('h3');
+    const price = document.createElement("p");
+    const btn = document.createElement("button");
+    const eliminar = document.createElement("button");
+    imgCard.setAttribute('src',imgProduct);
+    imgCard.setAttribute('alt',nameProduct);
+    title.textContent = nameProduct;
+    price.textContent = priceProduct;
+    eliminar.textContent = "X";
+    btn.textContent = "Comprar"
+    main.appendChild(card);
+    card.appendChild(title);
+    card.appendChild(eliminar);
+    card.appendChild(imgCard);
+    card.appendChild(price);
+    card.appendChild(btn);
+    card.classList.add("card");
+    imgCard.classList.add("img-card");
+    title.classList.add("p-title");
+    btn.classList.add("btn-add");
+    price.classList.add("p-price");
+    eliminar.classList.add("delete-btn");
+  // card.classList.remove("cardPrueba")
+}
+
+
+// products.map(element => { console.log(element.name) });
+
+eliminar.addEventListener("click", deleteCard)
+
+function deleteCard(){
+  card.removeChild(price)
+}
+
+
+
+
